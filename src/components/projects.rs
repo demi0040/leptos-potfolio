b56f9dea615042d967b77a1cf7455ce68b6ced37
Ihsan AbuYusr <ihsandemir.y@gmail.com>
2024-06-12 12:35:48 -0400

    Updated projects titles

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -9,153 +9,153 @@ pub fn Projects() -> impl IntoView {
                 <div class="splide__track">
                     <ul class="splide__list">
                         <li class="splide__slide">
-                            <h3>"Project 1"</h3>
+                            <h3>"Treemap Diagram"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 1"
+                                title="Treemap Diagram"
                                 src="https://codepen.io/demi0040/embed/yLqJjEM?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/yLqJjEM">Project 1</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/yLqJjEM">Treemap Diagram</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 2"</h3>
+                            <h3>"Choropleth Map"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 2"
+                                title="Choropleth Map"
                                 src="https://codepen.io/demi0040/embed/GRBqrrQ?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/GRBqrrQ">Project 2</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/GRBqrrQ">Choropleth Map</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 3"</h3>
+                            <h3>"Heat Map"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 3"
+                                title="Heat Map"
                                 src="https://codepen.io/demi0040/embed/VwBjwyr?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/VwBjwyr">Project 3</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/VwBjwyr">Heat Map</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 4"</h3>
+                            <h3>"Scatterplot Graph"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 4"
+                                title="Scatterplot Graph"
                                 src="https://codepen.io/demi0040/embed/poZgMvR?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/poZgMvR">Project 4</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/poZgMvR">Scatterplot Graph</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 5"</h3>
+                            <h3>"Bar Chart"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 5"
+                                title="Bar Chart"
                                 src="https://codepen.io/demi0040/embed/PoBqMaK?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/PoBqMaK">Project 5</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/PoBqMaK">Bar Chart</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 6"</h3>
+                            <h3>"25 + 5 Clock"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 6"
+                                title="25 + 5 Clock"
                                 src="https://codepen.io/demi0040/embed/VwBLJyR?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/VwBLJyR">Project 6</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/VwBLJyR">25 + 5 Clock</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 7"</h3>
+                            <h3>"Calculator-React"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 7"
+                                title="Calculator-React"
                                 src="https://codepen.io/demi0040/embed/XWBbQQr?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/XWBbQQr">Project 7</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/XWBbQQr">Calculator-React</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 8"</h3>
+                            <h3>"Drum Machine"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 8"
+                                title="Drum Machine"
                                 src="https://codepen.io/demi0040/embed/qBozYBJ?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/qBozYBJ">Project 8</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/qBozYBJ">Drum Machine</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 9"</h3>
+                            <h3>"Markdown Previewer"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 9"
+                                title="Markdown Previewer"
                                 src="https://codepen.io/demi0040/embed/PoRrwKG?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/PoRrwKG">Project 9</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/PoRrwKG">Markdown Previewer</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                         <li class="splide__slide">
-                            <h3>"Project 10"</h3>
+                            <h3>"Quote Machine"</h3>
                             <iframe
                                 height="500"
                                 style="width: 100%;"
                                 scrolling="no"
-                                title="Project 10"
+                                title="Quote Machine"
                                 src="https://codepen.io/demi0040/embed/WNzYQja?default-tab=result"
                                 frameborder="no"
                                 loading="lazy"
                                 allowtransparency="true"
                                 allowfullscreen="true">
-                                See the Pen <a href="https://codepen.io/demi0040/pen/WNzYQja">Project 10</a> by Your Name (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
+                                See the Pen <a href="https://codepen.io/demi0040/pen/WNzYQja">Quote Machine</a> by Ihsan Demir (<a href="https://codepen.io/demi0040">@demi0040</a>) on <a href="https://codepen.io">CodePen</a>.
                             </iframe>
                         </li>
                     </ul>
